602a9884aaa12efa17fc3a38cb6195993972cf07
Victor CG <ubicuosoft@gmail.com>
2025-07-08 13:53:11 -0400

    add projects mockito and assertj

---
+++ project test v1 assertJ/src/main/java/com/ubicuosoft/mock/ProjectTestV1MockApplication.java
@@ -0,0 +1,11 @@
+package com.ubicuosoft.mock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectTestV1MockApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectTestV1MockApplication.class, args);
+    }
+}
