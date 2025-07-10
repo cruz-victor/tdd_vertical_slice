acd35b201b76913bd8a36d590a5ccc06b5e33f94
Victor CG <ubicuosoft@gmail.com>
2025-07-10 02:32:21 -0400

    add test deberiaObtenerUsuarioPorId() controller

--- project test v1 mockito/src/test/java/com/ubicuosoft/mockito/ejemplosvarios/UsuarioControllerTest.java
+++ project test v1 mockito/src/test/java/com/ubicuosoft/mockito/ejemplosvarios/UsuarioControllerTest.java
@@ -1,6 +1,8 @@
 package com.ubicuosoft.mockito.ejemplosvarios;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
