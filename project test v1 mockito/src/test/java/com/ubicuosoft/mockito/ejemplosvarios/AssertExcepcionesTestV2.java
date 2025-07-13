132d630500ff1833d800db9a0a21950029eb836c
Victor CG <ubicuosoft@gmail.com>
2025-07-13 13:26:22 -0400

    add test asserts

--- project test v1 mockito/src/test/java/com/ubicuosoft/mockito/ejemplosvarios/AssertExcepcionesTestV2.java
+++ project test v1 mockito/src/test/java/com/ubicuosoft/mockito/ejemplosvarios/AssertExcepcionesTestV2.java
@@ -1,12 +1,19 @@
 package com.ubicuosoft.mockito.ejemplosvarios;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.Assertions.catchThrowable;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+@ExtendWith(MockitoExtension.class)
 public class AssertExcepcionesTestV2 {
+
+    @InjectMocks
+    private UsuarioService usuarioService;
 
     @Test
     void deberiaVerificarMensaje_conAssertThatThrownBy() {
@@ -46,5 +53,21 @@ public class AssertExcepcionesTestV2 {
         assertThat(thown)
                 .isInstanceOf(NullPointerException.class)
                 .hasMessageContaining("nombre");
+    }
+
+    @Test
+    void deberiaVerificarMensajeDeMetodoReal_conAssertThatThrownBy(){
+        assertThatThrownBy(()->usuarioService.eliminarPorId(null))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("ID no puede ser null");
+    }
+
+    @Test
+    void deberiaVerificarMensajeDeMetodoReal_conCatchThrowable(){
+        Throwable lanzado=catchThrowable(()->usuarioService.eliminarPorId(null));
+
+        assertThat(lanzado)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("ID no puede ser null");
     }
 }
