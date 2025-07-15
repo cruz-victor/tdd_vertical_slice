add3a4df9986448e31418fc49ada61c5065bc5b8
Victor CG <ubicuosoft@gmail.com>
2025-07-15 10:24:45 -0400

    add test asserts mockTest

--- project test v1 mockito/src/test/java/com/ubicuosoft/mockito/ejemplosvarios/MockVerificarLlamadaTest.java
+++ project test v1 mockito/src/test/java/com/ubicuosoft/mockito/ejemplosvarios/MockVerificarLlamadaTest.java
@@ -6,6 +6,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
@@ -20,5 +22,18 @@ public class MockVerificarLlamadaTest {
     void deberiaLlamarAlMetodoGuardar() {
         servicio.procesar();
         verify(repositorio).guardar();
+    }
+
+    @Test
+    void deberiaGuardarDosVeces(){
+        servicio.procesar();
+        servicio.procesar();
+        verify(repositorio,times(2)).guardar();
+    }
+
+    @Test
+    void deberiaLlamarGuardarConNombreEspecifico(){
+        servicio.registrar("Victor");
+        verify(repositorio).guardar(eq("Victor"));
     }
 }
