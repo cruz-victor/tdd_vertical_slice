6e19932aa859b6dd32a72680445cbeb118b50fae
Victor CG <ubicuosoft@gmail.com>
2026-01-10 17:25:29 -0400

    add proyect spring boot

--- project test v1 mockito/src/test/java/com/ubicuosoft/mockito/ejemplosvarios/AssertTest.java
+++ project test v1 mockito/src/test/java/com/ubicuosoft/mockito/ejemplosvarios/AssertTest.java
@@ -28,14 +28,22 @@ public class AssertTest {
 
     @Test
     void deberiaAfirmarValoresBooleanos() {
+        //---Given
         boolean activo = true;
+        //---When
+
+        //---Then
         assertThat(activo).isTrue();
         assertThat(!activo).isFalse();
     }
 
     @Test
     void deberiaAfirmarListasColecciones() {
+        //---Given
         List<String> nombres = List.of("Ana", "Luis", "Carlos");
+        //---When
+
+        //---Then
         assertThat(nombres).hasSize(3);
         assertThat(nombres).contains("Ana", "Luis");
         assertThat(nombres).doesNotContain("Pedro");
@@ -45,7 +53,11 @@ public class AssertTest {
 
     @Test
     void deberiaAfirmarMapas() {
+        //---Given
         Map<String, Integer> edades = Map.of("Ana", 20, "Luis", 30);
+        //---When
+
+        //---Then
         assertThat(edades).containsKey("Ana");
         assertThat(edades).containsEntry("Luis", 30);
         assertThat(edades).doesNotContainKey("Pedro");
@@ -53,7 +65,11 @@ public class AssertTest {
 
     @Test
     void deberiaAfirmarOptional() {
+        //---Given
         Optional<String> nombre = Optional.of("Victor");
+        //---When
+
+        //---Then
         assertThat(nombre).isPresent();
         assertThat(nombre).hasValue("Victor");
         assertThat(nombre.get()).startsWith("Vic");
@@ -61,13 +77,20 @@ public class AssertTest {
 
     @Test
     void deberiaAfirmarObjetosPersonalizados() {
+        //---Given
         Usuario usuario = new Usuario(1L, "Victor", "victor@gmail.com",15);
+        //---When
+
+        //---Then
         assertThat(usuario.getNombre()).isEqualTo("Victor");
         assertThat(usuario.getEmail()).contains("@");
     }
 
     @Test
     void deberiaAfirmarExcepciones() {
+        //---Given
+        //---When
+        //---Then
         assertThatThrownBy(() -> {
             throw new IllegalArgumentException("ID invalido");
         }).isInstanceOf(IllegalArgumentException.class)
@@ -76,26 +99,37 @@ public class AssertTest {
 
     @Test
     void deberiaAfirmarString() {
+        //---Given
         String saludo = "Hola mundo";
+        //---When
+
+        //---Then
         assertThat(saludo).startsWith("Hola");
         assertThat(saludo).endsWith("mundo");
         assertThat(saludo).contains("la mundo");
         assertThat(saludo).isEqualToIgnoringCase("hola mundo");
-
         assertThat(saludo).startsWith("Hola").endsWith("mundo").contains("la mundo").isEqualToIgnoringCase("hola mundo");
     }
 
     @Test
     void deberiaAfirmarFechasTiempos() {
+        //---Given
         LocalDate hoy = LocalDate.now();
         LocalDate ayer = hoy.minusDays(1);
+        //---When
+
+        //---Then
         assertThat(hoy).isAfter(ayer);
         assertThat(ayer).isBefore(hoy);
     }
 
     @Test
     void deberiaAfirmarArrays() {
+        //---Given
         String[] colores = {"Rojo", "Amarillo", "Verde"};
+        //---When
+
+        //---Then
         assertThat(colores).contains("Verde");
         assertThat(colores).startsWith("Rojo");
         assertThat(colores).hasSize(3);
@@ -103,23 +137,39 @@ public class AssertTest {
 
     @Test
     void deberiaAfirmarNulls() {
+        //---Given
         String nombre = null;
+        //---When
+
+        //---Then
         assertThat(nombre).isNull();
 
+        //---Given
         nombre = "Victor";
+        //---When
+
+        //---Then
         assertThat(nombre).isNotNull();
     }
 
     @Test
     void deberiaAfirmarClasesTipos() {
+        //---Given
         Object valor = "Hola";
+        //---When
+
+        //---Then
         assertThat(valor).isInstanceOf(String.class);
         //assertThat(valor.getClass()).hasSimpleName("String"); //not working
     }
 
     @Test
     void deberiaAfirmarLambdas() {
+        //---Given
         List<Integer> numeros = List.of(2, 4, 6, 8);
+        //---When
+
+        //---Then
         assertThat(numeros).allMatch(n -> n % 2 == 0); //coincide todos pares
         assertThat(numeros).noneMatch(n -> n < 0); //ninguno coincide negativo
     }
